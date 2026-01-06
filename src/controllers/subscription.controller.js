7214d3c4e8b5d5323cbaac825af25e291d35074c
arijitk930 <arijitk930@gmail.com>
2026-01-06 21:06:41 +0530

    feat: implement subscription toggle functionality; add validation for channel and subscriber IDs, improve error handling, and enhance response messages

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -4,20 +4,101 @@ import { Subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { validateMongoId } from "../utils/validateMongoId.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
   // TODO: toggle subscription
+
+  validateMongoId(channelId, "Channel ID");
+
+  const channel = User.exists({ _id: channelId });
+
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  const existingSubscription = await Subscription.findOne({
+    subscriber: req.user?._id,
+    channel: channelId,
+  });
+
+  if (existingSubscription) {
+    await existingSubscription.deleteOne();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Channel unsubscribed successfully"));
+  }
+
+  const subscription = await Subscription.create({
+    subscriber: req.user?._id,
+    channel: channelId,
+  });
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, subscription, "Channel subscribed successfully")
+    );
 });
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
+  validateMongoId(channelId, "Channel ID");
+
+  const channel = await User.exists({ _id: channelId });
+
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  const subscribers = await Subscription.find({ channel: channelId })
+    .populate("subscriber", "username fullName avatar")
+    .sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { totalSubscribers: subscribers.length, subscribers },
+        "Subscribers fetched successfully"
+      )
+    );
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+
+  validateMongoId(subscriberId, "Subscriber ID");
+
+  const user = await User.exists({ _id: subscriberId });
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  if (!req.user?._id.equals(subscriberId)) {
+    throw new ApiError(403, "Not authorised to view this user's subscriptions");
+  }
+
+  const subscriptions = await Subscription.find({ subscriber: _id })
+    .populate("channel", "username fullName avatar")
+    .sort({ createdAt: -1 });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalSubscriptions: subscriptions.length,
+        subscriptions,
+      },
+      "Subscriptions fetched successfully"
+    )
+  );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
